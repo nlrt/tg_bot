9d67e172c9688aef059f0c55b9859284f0cc0503
nlrt <95478839+nlrt@users.noreply.github.com>
2024-01-27 23:21:58 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+import os
+
+TELEGRAM_BOT_TOKEN = "6984923602:AAEnTylrIQdhj0K57mreVXnjvG1qUeTpaJc"
+CREDENTIALS_FILE = "credentials.json"
+SPREADSHEET_ID = "1gsDKDLMOlYKB9dUvotLxg1slpsQ9KiF9yS60-_I-ho4"
+PASSWORD = "515746"
